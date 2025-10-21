399d0a169ea58f7754757eae0c8e51e8cfd25875
26blevine <26blevine@gannacademy.org>
2025-10-21 15:41:31 -0400

    ok

--- .ipynb_checkpoints/series_practice_1-checkpoint.py
+++ .ipynb_checkpoints/series_practice_1-checkpoint.py
@@ -5,8 +5,35 @@ sales = [1500, 2300, 2100, 2400, 1800, 2200, 2050]
 
 # Creating a Pandas Series with dates as the index
 sales_series = pd.Series(data=sales, index=dates)
-#thresh=
-#ind=
+thresh=2000
+ind=[1, 3, 5]
+
+
+def get_total_sales(sales_data):
+    print(sales_data.sum())
+    pass
+
+
+def get_date_with_highest_sales(sales_data):
+    print(sales_data.idxmax())
+    pass
+
+
+
+def get_average_sales(sales_data):
+    print(sales_data.mean())
+    pass
+
+
+def get_days_with_sales_above(sales_data, threshold):
+    print(sales_data[sales_data > threshold])
+    pass
+
+
+def get_sales_on_selected_days(sales_data, indices):
+    print(sales_data[indices])
+
+
 get_total_sales(sales_series)
 get_date_with_highest_sales(sales_series)
 get_average_sales(sales_series)
@@ -14,24 +41,3 @@ get_days_with_sales_above(sales_series, thresh)
 get_sales_on_selected_days(sales_series, ind)
 
 
-
-def get_total_sales(sales_data):
-    
-    pass
-
-
-def get_date_with_highest_sales(sales_data):
-    pass
-
-
-
-def get_average_sales(sales_data):
-    pass
-
-
-def get_days_with_sales_above(sales_data, threshold):
-    pass
-
-
-def get_sales_on_selected_days(sales_data, indices):
-    pass
